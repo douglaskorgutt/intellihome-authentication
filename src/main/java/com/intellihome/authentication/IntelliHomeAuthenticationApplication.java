86633233b3b2192f0133d622a7c16ec2e81c76d4
douglas.korgut@sita.aero <douglas.korgut@sita.aero>
2023-12-24 13:34:46 -0300

    Rename main class to IntelliHomeAuthenticationApplication

--- src/main/java/com/intellihome/authentication/IntelliHomeAuthenticationApplication.java
+++ src/main/java/com/intellihome/authentication/IntelliHomeAuthenticationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntellihomeAuthenticationApplication {
+public class IntelliHomeAuthenticationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IntellihomeAuthenticationApplication.class, args);
+        SpringApplication.run(IntelliHomeAuthenticationApplication.class, args);
     }
 
 }
