cfa31646c343ceb9697cc7ce70d4cf579ee6695a
douglas.korgut@sita.aero <douglas.korgut@sita.aero>
2023-12-24 13:49:06 -0300

    create sample login api

---
+++ src/main/java/com/intellihome/authentication/controller/v1/LoginControllerV1.java
@@ -0,0 +1,13 @@
+package com.intellihome.authentication.controller.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginControllerV1 {
+
+    @GetMapping("/login")
+    public String login() {
+        return "User has successfully logged in";
+    }
+}
